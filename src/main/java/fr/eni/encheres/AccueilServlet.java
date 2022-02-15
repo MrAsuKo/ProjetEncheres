e000bb97740afb4b589edc8f3e722951b6eeba0e
Lénaïc Jousselin <lenaic.jousselin2021@campus-eni.fr>
2022-02-15 09:37:16 +0100

    lancement projet generale

--- src/main/java/fr/eni/encheres/AccueilServlet.java
+++ src/main/java/fr/eni/encheres/AccueilServlet.java
@@ -1,6 +1,8 @@
 package fr.eni.encheres;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,8 +28,8 @@ public class AccueilServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/Accueil.jsp");
+		rd.forward(request, response);
 	}
 
 	/**
