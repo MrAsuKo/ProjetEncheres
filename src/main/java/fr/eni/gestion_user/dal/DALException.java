873842ecabbb9bc5c6b38d4c022264043a3b1f95
Lénaïc Jousselin <lenaic.jousselin2021@campus-eni.fr>
2022-02-15 10:22:55 +0100

    Merge branch 'main' of https://github.com/MrAsuKo/ProjetEncheres.git into main

---
+++ src/main/java/fr/eni/gestion_user/dal/DALException.java
@@ -0,0 +1,14 @@
+package fr.eni.gestion_user.dal;
+
+public class DALException extends Exception{
+
+	/**
+	 * @param message
+	 */
+	public DALException(String message) {
+		super(message);
+	}
+	
+	
+
+}
