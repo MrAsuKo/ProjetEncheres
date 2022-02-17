9657e28aca966577ee7e1948186b80ea50c7fc9f
Lénaïc Jousselin <lenaic.jousselin2021@campus-eni.fr>
2022-02-17 17:27:47 +0100

    Merge branch 'main' of https://github.com/MrAsuKo/ProjetEncheres.git into main

--- src/main/java/fr/eni/gestion_user/dal/userDAOjdcImpl.java
+++ src/main/java/fr/eni/gestion_user/dal/userDAOjdcImpl.java
@@ -13,7 +13,7 @@ public class userDAOjdcImpl implements UserDAO {
 	private static final String SELECT_USEREMAIL = "SELECT email FROM UTILISATEURS WHERE email=?";
 	private static final String SELECT_USER = "SELECT no_utilisateur, pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur FROM UTILISATEURS WHERE pseudo=? and mot_de_passe=?";
 	private static final String DELETE_USER = "DELETE FROM UTILISATEURS WHERE pseudo=?";
-	private static final String UPDATE_USER ="UPDATE UTILISATEURS SET pseudo=?, nom=?, prenom=?, email=?, telephone=?, rue=?, code_postal=?, ville=? WHERE pseudo=? and mot_de_passe=?";
+	private static final String UPDATE_USER ="UPDATE UTILISATEURS SET  nom=?, prenom=?, email=?, telephone=?, rue=?, code_postal=?, ville=? WHERE pseudo=? and mot_de_passe=?";
 	
 	public boolean verif(User user) throws DALException {
 		Connection cnx = null;
@@ -112,21 +112,18 @@ public class userDAOjdcImpl implements UserDAO {
 		 try {
 			 cnx = ConnectionProvider.getConnection();
 			 PreparedStatement rqt = cnx.prepareStatement(UPDATE_USER);
-			 rqt.setString(9,user.getPseudo());
-			 rqt.setString(10,user.getMdp());
-			 ResultSet rs = rqt.executeQuery();
-			 System.out.println(user.getPseudo());
-			 if (rs.next()) {
-				rqt.setString(1, user.getPseudo());
-				rqt.setString(2, user.getNom());
-				rqt.setString(3, user.getPrenom());
-				rqt.setString(4, user.getEmail());
-				rqt.setString(5, user.getTelephone());
-				rqt.setString(6, user.getRue());
-				rqt.setString(7, user.getCp());
-				rqt.setString(8, user.getVille());
+			 rqt.setString(8,user.getPseudo());
+			 rqt.setString(9,user.getMdp());
+			 System.out.println("1 : " +user.getPseudo());
+				rqt.setString(1, user.getNom());
+				rqt.setString(2, user.getPrenom());
+				rqt.setString(3, user.getEmail());
+				rqt.setString(4, user.getTelephone());
+				rqt.setString(5, user.getRue());
+				rqt.setString(6, user.getCp());
+				rqt.setString(7, user.getVille());
 				rqt.executeUpdate();
-			 }
+			 
 		 } catch (SQLException e) {
 			 e.printStackTrace();
 		 }
